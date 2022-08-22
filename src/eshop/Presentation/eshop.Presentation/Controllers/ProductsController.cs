4fac68c71955df198e1a655e1e5c64505fa6743b
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-08-22 12:35:57 +0300

    structure for N-Tier & N-Layer  Client - Server Archtitectuer

---
+++ src/eshop/Presentation/eshop.Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eshop.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+        }
+    }
+}
